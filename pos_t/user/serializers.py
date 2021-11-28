a0fb518ca1c40705d1e80c044e917b971b1b95c5
logo1tip <evgeni.leschinski@gmail.com>
2021-11-28 21:12:28 +0300

    Comments

--- pos_t/user/serializers.py
+++ pos_t/user/serializers.py
@@ -2,10 +2,10 @@ from rest_framework.serializers import ModelSerializer
 from user.models import CustomUser
 
 
-
-
-
 class UserSerializer(ModelSerializer):
+
+    """ Сериализатор для модели пользователя"""
+
     class Meta:
         model = CustomUser
         fields = "__all__"
--- pos_t/user/views.py
+++ pos_t/user/views.py
@@ -1,18 +1,23 @@
 from user.models import CustomUser
 from user.serializers import UserSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
-from user.permissions import IsOwnerOrReadOnly
-from rest_framework import permissions
+from rest_framework import permissions 
+
 
 
 class UserList(generics.ListCreateAPIView):
+    
+    """ Представление для создания пользователя """
+    
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAdminUser]
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    """ Представление для получения, обновления и удаления пользователя """
+
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAdminUser]
